ceef01f279ae1b42c9ff53cc0e2f5071a9f53519
wakamori <wakamori111@gmail.com>
2012-03-21 16:54:17 +0900

    modify iframe

--- aspen/static/js/script.js
+++ aspen/static/js/script.js
@@ -7,20 +7,8 @@ $(function() {
         lineNumbers: true
     });
     $("#runbtn").click(function() {
-        $.ajax({
-            type: "POST",
-            url: "http://localhost/cgi-bin/konoha2js.k",
-            data: {
-                requestedType: "html",
-                source: editor.getValue()
-            },
-            success: function(msg) {
-                $("#body").html("<div class=\"modal-body\" id=\"body\"><iframe sandbox=\"allow-same-origin allow-forms allow-scripts\" src=\"" + msg.src + "\"></iframe></div>");
-            },
-            error: function(msg) {
-                alert("error: " + msg);
-            }
-        });
+        $("#sourceinput").val(editor.getValue());
+        $("#codeform").submit();
     });
 });
 
--- aspen/view/editor.cs
+++ aspen/view/editor.cs
@@ -1,10 +1,15 @@
 <?cs include:"header.cs" ?>
-<div class="modal fade" id="result">
+<form action="/cgi-bin/konoha2js.k" method="post" target="resultframe" id="codeform">
+  <input type="hidden" name="requestedType" value="html">
+  <input type="hidden" name="source" value="" id="sourceinput">
+</form>
+<div class="modal fade" id="resultwindow">
   <div class="modal-header">
     <a class="close" data-dismiss="modal">&times;</a>
     <h3>Result</h3>
   </div>
   <div class="modal-body" id="body">
+    <iframe name="resultframe"></iframe>
   </div>
   <div class="modal-footer">
     <a href="#" class="btn" data-dismiss="modal">Close</a>
@@ -12,7 +17,7 @@
 </div>
 <div class="container">
   <div class="btn-group">
-    <button class="btn btn-primary" data-toggle="modal" href="#result" id="runbtn">Run</button>
+    <button class="btn btn-primary" data-toggle="modal" href="#resultwindow" id="runbtn">Run</button>
   </div>
   <textarea id="editor">// Your code goes here.
 print "hello, world";</textarea>
