b827c03b3280f38e9e64140dd1cdc565ff29d1eb
wakamori <wakamori111@gmail.com>
2012-03-28 04:37:55 +0900

    add Home link in navigation bar

--- aspen/view/header.cs
+++ aspen/view/header.cs
@@ -72,6 +72,7 @@
               <li id="fat-menu" class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user icon-white"></i> <?cs var:ID ?><b class="caret"></b></a>
                 <ul class="dropdown-menu">
+                  <li><a href="<?cs var:BASE_URL ?>/<?cs var:ID ?>"><i class="icon-home"></i> Home</a></li>
                   <li><a href="#" id="signoutbtn">Sign out</a></li>
                 </ul>
               </li>
