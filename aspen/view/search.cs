911d8a40a8de0d83cf52345eceb8677c695b8187
wakamori <wakamori111@gmail.com>
2012-03-29 08:41:20 +0900

    add search view

---
+++ aspen/view/search.cs
@@ -0,0 +1,32 @@
+<?cs include:"header.cs" ?>
+<h1>Search text</h1>
+<form action="<?cs var:BASE_URL ?>/action/search" class="well form-search" method="post">
+  <input type="text" name="text" class="input-xlarge search-query" value="<?cs var:Search.Text ?>">
+  <button type="submit" class="btn">Search</button>
+</form>
+<h1>Search result</h1>
+<div class="row-fluid">
+  <div class="span6">
+    <h2>User list</h2>
+    <table class="table table-bordered table-stripped">
+      <thead>
+        <tr><th>User Name</th></tr>
+      </thead>
+      <tbody>
+        <?cs each:item = Result.Users ?><tr><td><?cs var:item ?></td></tr><?cs /each ?>
+      </tbody>
+    </table>
+  </div>
+  <div class="span6">
+    <h2>Code list</h2>
+    <table class="table table-bordered table-stripped">
+      <thead>
+        <tr><th>Code Name</th></tr>
+      </thead>
+      <tbody>
+        <?cs each:item = Result.Codes ?><tr><td><a href="<?cs var:BASE_URL ?>/<?cs var:item.user ?>/<?cs var:item.id ?>"><?cs var:item.user ?>/<?cs var:item.name ?></a></td></tr><?cs /each ?>
+      </tbody>
+    </table>
+  </div>
+</div>
+<?cs include:"footer.cs" ?>
