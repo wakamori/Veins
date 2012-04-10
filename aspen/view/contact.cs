300b7f6d6a239afe581f082b6721981c2c04c6b1
wakamori <wakamori111@gmail.com>
2012-04-10 21:55:39 +0900

    add contact

---
+++ aspen/view/contact.cs
@@ -0,0 +1,20 @@
+<?cs include:"header.cs" ?>
+<p>
+  <a href="" class="btn"><i class="icon-pencil"></i> </a>
+</p>
+<h1>All Contacts</h1>
+<table class="table">
+  <thead>
+    <tr><th>Name</th><th>Title</th><th>Date</th></tr>
+  </thead>
+  <tbody>
+    <?cs each:item = Thread ?>
+    <tr id="<?cs var:item.Id ?>">
+      <td><?cs var:item.Name ?></td>
+      <td><a href="<?cs var:BASE_URL ?>/contact/<?cs var:item.Id ?>"><?cs var:item.Title ?></a></td>
+      <td><?cs var:item.Date ?></td>
+    </tr>
+    <?cs /each ?>
+  </tbody>
+</table>
+<?cs include:"footer.cs" ?>
