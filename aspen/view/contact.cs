312bd49e9a8fa779a82c1002fa6f3c6d59b48165
wakamori <wakamori111@gmail.com>
2012-04-11 12:04:53 +0900

    add contact form

--- aspen/view/contact.cs
+++ aspen/view/contact.cs
@@ -1,17 +1,19 @@
 <?cs include:"header.cs" ?>
+<?cs if:ID ?>
 <p>
-  <a class="btn"><i class="icon-pencil"></i> Post</a>
+  <a href="<?cs var:BASE_URL ?>/contact/new" class="btn"><i class="icon-pencil"></i> Post</a>
 </p>
+<?cs /if ?>
 <h1>All Contacts</h1>
 <table class="table">
   <thead>
-    <tr><th>Name</th><th>Title</th><th>Date</th></tr>
+    <tr><th>Title</th><th>Name</th><th>Date</th></tr>
   </thead>
   <tbody>
-    <?cs each:item = Thread ?>
+    <?cs each:item = Threads ?>
     <tr id="<?cs var:item.Id ?>">
-      <td><?cs var:item.Name ?></td>
       <td><a href="<?cs var:BASE_URL ?>/contact/<?cs var:item.Id ?>"><?cs var:item.Title ?></a></td>
+      <td><?cs var:item.Name ?></td>
       <td><?cs var:item.Date ?></td>
     </tr>
     <?cs /each ?>
