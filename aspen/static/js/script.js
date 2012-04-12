fb62332594df83711881060795450d5889d6c174
wakamori <wakamori111@gmail.com>
2012-04-12 13:42:03 +0900

    arrange google plus button

--- aspen/static/js/script.js
+++ aspen/static/js/script.js
@@ -55,6 +55,25 @@
     });
 })(jQuery);
 
+// twitter
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+// facebook
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// google plus
+(function() {
+  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+  po.src = 'https://apis.google.com/js/plusone.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
+
 $(function() {
     if ($("#editor1")[0] != undefined) {
         var editor1 = CodeMirror.fromTextArea($("#editor1")[0], {
--- aspen/view/viewer.cs
+++ aspen/view/viewer.cs
@@ -1,15 +1,17 @@
 <?cs include:"header.cs" ?>
 <form action="/aspen/<?cs var:User.Name ?>/<?cs var:User.Id ?>/html" method="get" target="resultframe" id="codeform" class="hideform"></form>
-<ul class="breadcrumb">
-  <li>
-    <a href="<?cs var:BASE_URL ?>/<?cs var:User.Name ?>"><?cs var:User.Name ?></a> <span class="divider">/</span>
-  </li>
-  <li class="active"><?cs var:Code.Name ?></li>
-</ul>
 <div class="container-fluid">
+  <ul class="breadcrumb">
+    <li>
+      <a href="<?cs var:BASE_URL ?>/<?cs var:User.Name ?>"><?cs var:User.Name ?></a> <span class="divider">/</span>
+    </li>
+    <li class="active"><?cs var:Code.Name ?></li>
+  </ul>
+  <p>
   <div class="btn-group">
     <button class="btn btn-primary" data-toggle="modal" href="#resultwindow" id="runbtn">Run</button>
   </div>
+  </p>
   <div class="row-fluid">
     <div class="span6">
       <div class="tabbable">
@@ -48,9 +50,19 @@
     </div>
   </div>
   <div class="well">
+    <!-- Twitter -->
     <a href="https://twitter.com/share" class="twitter-share-button" data-text="<?cs var:User.Name ?> / <?cs var:Code.Name ?>" data-hashtags="aspen">Tweet</a>
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-    <div class="fb-like" data-href="<?cs var:BASE_URL ?>/<?cs var:User.Name ?>/<?cs var:User.Id ?>" data-send="false" data-layout="button_count" data-width="110" data-show-faces="true"></div>
+    <div class="fb-like" data-href="<?cs var:BASE_URL ?>/<?cs var:User.Name ?>/<?cs var:User.Id ?>" data-send="false" data-layout="button_count" data-width="110" data-show-faces="false"></div>
+    <div class="g-plusone" data-size="medium"></div>
+    <!--<div data-plugins-type="mixi-favorite" data-service-key="..."></div>
+    <script type="text/javascript">
+      (function(d) {
+        var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true;
+        s.src = '//static.mixi.jp/js/plugins.js#lang=ja';
+        d.getElementsByTagName('head')[0].appendChild(s);
+      })(document);
+    </script>
+    -->
   </div>
 </div>
 <?cs include:"footer.cs" ?>
