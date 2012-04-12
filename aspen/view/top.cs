25fedcff031f8c34835e362ec13df41a0cd854d3
wakamori <wakamori111@gmail.com>
2012-04-13 05:30:02 +0900

    modify top page messages

--- aspen/view/top.cs
+++ aspen/view/top.cs
@@ -9,22 +9,18 @@
   </p>
 </div>
 
-<!-- Example row of columns -->
 <div class="row">
   <div class="span4">
-    <h2>Heading</h2>
-     <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-    <p><a class="btn" href="#">View details &raquo;</a></p>
+    <h2>Online code hosting</h2>
+     <p>You can edit your code with in-browser code editor. You can access your scripts from anyware.</p>
   </div>
   <div class="span4">
-    <h2>Heading</h2>
-     <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-    <p><a class="btn" href="#">View details &raquo;</a></p>
+    <h2>Static type checker</h2>
+     <p>Scripts are converted from Konoha to JavaScript on a server side, and they are runned on client side. Since konoha is a statically typed scripting language, a type checker can detect type errors without running a script.</p>
  </div>
   <div class="span4">
-    <h2>Heading</h2>
-    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-    <p><a class="btn" href="#">View details &raquo;</a></p>
+    <h2>Social coding</h2>
+    <p>Share your code with others.</p>
   </div>
 </div>
 <?cs include:"footer.cs" ?>
