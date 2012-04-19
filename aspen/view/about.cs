55b8618037bbe5cee7a0a35721fc4cfc894cece7
wakamori <wakamori111@gmail.com>
2012-04-19 18:56:27 +0900

    add what's new in About page

--- aspen/view/about.cs
+++ aspen/view/about.cs
@@ -1,6 +1,35 @@
 <?cs include:"header.cs" ?>
 <h1>About Aspen</h1>
 <p>Aspen is an online Konoha development environment.</p>
+<h1>What's New</h1>
+<script charset="utf-8" src="http://widgets.twimg.com/j/2/widget.js"></script>
+<script>
+new TWTR.Widget({
+  version: 2,
+  type: 'profile',
+  rpp: 4,
+  interval: 30000,
+  width: 'auto',
+  height: 300,
+  theme: {
+    shell: {
+      background: '#2c2c2c',
+      color: '#ffffff'
+    },
+    tweets: {
+      background: '#ffffff',
+      color: '#333333',
+      links: '#0088cc'
+    }
+  },
+  features: {
+    scrollbar: true,
+    loop: false,
+    live: true,
+    behavior: 'default'
+  }
+}).render().setUser('ynualgo2012').start();
+</script>
 <h1>Links</h1>
 <dl>
 <?cs each:item = Links ?>
