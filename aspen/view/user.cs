c0ee3e20aa8c5bddeff4ce37a1023164f18675d6
wakamori <wakamori111@gmail.com>
2012-04-20 22:28:16 +0900

    modify user view

--- aspen/view/user.cs
+++ aspen/view/user.cs
@@ -1,40 +1,22 @@
 <?cs include:"header.cs" ?>
-<div class="container-fluid">
-  <div class="row-fluid">
-    <div class="span2">
-      <table class="table table-bordered">
-        <tbody>
-          <?cs each:item = Info ?>
-          <tr>
-            <th><?cs var:item.key ?></th>
-            <td><?cs var:item.value ?></td>
-          </tr>
-          <?cs /each ?>
-        </tbody>
-      </table>
-    </div>
-    <div class="span10">
-      <?cs if:Myself ?>
-      <p>
-        <a href="<?cs var:BASE_URL ?>/action/create" class="btn"><i class="icon-edit"></i> Create</a>
-      </p>
-      <?cs /if ?>
-      <h1>My Code</h1>
-      <table class="table">
-        <thead>
-          <tr><th>Name</th><?cs if:Myself ?><th>Actions</th><?cs /if ?><th>Date</th></tr>
-        </thead>
-        <tbody>
-          <?cs each:item = Code ?>
-          <tr id="<?cs var:item.Id ?>">
-            <td><a href="<?cs var:BASE_URL ?>/<?cs var:User ?>/<?cs var:item.Id ?>"><?cs var:item.Name ?></a></td>
-            <?cs if:Myself ?><td><a href="<?cs var:BASE_URL ?>/<?cs var:User ?>/<?cs var:item.Id ?>/edit" class="btn btn-mini">Edit</a> <button class="btn btn-mini btn-danger confirm">Delete</button></td><?cs /if ?>
-            <td><?cs var:item.Date ?></td>
-          </tr>
-          <?cs /each ?>
-        </tbody>
-      </table>
-    </div>
-  </div>
-</div>
+<?cs if:Myself ?>
+<p>
+  <a href="<?cs var:BASE_URL ?>/action/create" class="btn"><i class="icon-edit"></i> Create</a>
+</p>
+<?cs /if ?>
+<h1><?cs var:User ?>&#39;s Code</h1>
+<table class="table">
+  <thead>
+    <tr><th>Name</th><?cs if:Myself ?><th>Actions</th><?cs /if ?><th>Date</th></tr>
+  </thead>
+  <tbody>
+    <?cs each:item = Code ?>
+    <tr id="<?cs var:item.Id ?>">
+      <td><a href="<?cs var:BASE_URL ?>/<?cs var:User ?>/<?cs var:item.Id ?>"><?cs var:item.Name ?></a></td>
+      <?cs if:Myself ?><td><a href="<?cs var:BASE_URL ?>/<?cs var:User ?>/<?cs var:item.Id ?>/edit" class="btn btn-mini">Edit</a> <button class="btn btn-mini btn-danger confirm">Delete</button></td><?cs /if ?>
+      <td><?cs var:item.Date ?></td>
+    </tr>
+    <?cs /each ?>
+  </tbody>
+</table>
 <?cs include:"footer.cs" ?>
