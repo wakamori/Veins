58f1cd9702d5faff4ceee43dbcf425fb1a35e843
wakamori <wakamori111@gmail.com>
2012-04-25 21:40:27 +0900

    change editor layout

--- aspen/view/editor.cs
+++ aspen/view/editor.cs
@@ -12,46 +12,38 @@
     <a href="#" class="btn" data-dismiss="modal">Close</a>
   </div>
 </div>
-<div class="container-fluid">
-  <div class="row-fluid">
-    <div class="span2">
-      <div class="btn-group">
-        <button class="btn btn-primary" data-toggle="modal" href="#resultwindow" id="runbtn">Run</button>
-        <button class="btn" id="checkbtn">Check</button>
-        <button class="btn" id="savebtn">Save</button>
-      </div>
+<ul class="breadcrumb">
+  <li>
+    <a href="<?cs var:BASE_URL ?>/<?cs var:User.Name ?>"><?cs var:User.Name ?></a> <span class="divider">/</span>
+  </li>
+  <li id="codename">
+    <a href="<?cs var:BASE_URL ?>/<?cs var:User.Name ?>/<?cs var:User.Id ?>"><?cs var:Code.Name ?></a>
+  </li>
+</ul>
+<div class="btn-toolbar">
+  <button class="btn btn-primary" data-toggle="modal" href="#resultwindow" id="runbtn">Run</button>
+  <button class="btn btn-warning" id="checkbtn">Check</button>
+  <button class="btn btn-success" id="savebtn">Save</button>
+</div>
+<div class="tabbable">
+  <ul class="nav nav-tabs">
+    <li><a href="#1" data-toggle="tab" id="tab1">Readme</a></li>
+    <li class="active"><a href="#2" data-toggle="tab" id="tab2">konoha</a></li>
+    <li class="hidden"><a href="#3" data-toggle="tab" id="tab3">konoha</a></li>
+    <li><a href="#4" data-toggle="tab" id="tab4">HTML</a></li>
+  </ul>
+  <div class="tab-content">
+    <div class="tab-pane" id="1">
+      <textarea id="editor1"><?cs var:Code.Body.Readme ?></textarea>
     </div>
-    <div class="span10">
-      <ul class="breadcrumb">
-        <li>
-          <a href="<?cs var:BASE_URL ?>/<?cs var:User.Name ?>"><?cs var:User.Name ?></a> <span class="divider">/</span>
-        </li>
-        <li id="codename">
-          <a href="<?cs var:BASE_URL ?>/<?cs var:User.Name ?>/<?cs var:User.Id ?>"><?cs var:Code.Name ?></a>
-        </li>
-      </ul>
-      <div class="tabbable">
-        <ul class="nav nav-tabs">
-          <li><a href="#1" data-toggle="tab" id="tab1">Readme</a></li>
-          <li class="active"><a href="#2" data-toggle="tab" id="tab2">konoha</a></li>
-          <li class="hidden"><a href="#3" data-toggle="tab" id="tab3">konoha</a></li>
-          <li><a href="#4" data-toggle="tab" id="tab4">HTML</a></li>
-        </ul>
-        <div class="tab-content">
-          <div class="tab-pane" id="1">
-            <textarea id="editor1"><?cs var:Code.Body.Readme ?></textarea>
-          </div>
-          <div class="tab-pane active" id="2">
-            <textarea id="editor2"><?cs var:Code.Body.Js ?></textarea>
-          </div>
-          <div class="tab-pane" id="3">
-            <textarea id="editor3"><?cs var:Code.Body.Ks ?></textarea>
-          </div>
-          <div class="tab-pane" id="4">
-            <textarea id="editor4"><?cs var:Code.Body.Html ?></textarea>
-          </div>
-        </div>
-      </div>
+    <div class="tab-pane active" id="2">
+      <textarea id="editor2"><?cs var:Code.Body.Js ?></textarea>
+    </div>
+    <div class="tab-pane" id="3">
+      <textarea id="editor3"><?cs var:Code.Body.Ks ?></textarea>
+    </div>
+    <div class="tab-pane" id="4">
+      <textarea id="editor4"><?cs var:Code.Body.Html ?></textarea>
     </div>
   </div>
 </div>
